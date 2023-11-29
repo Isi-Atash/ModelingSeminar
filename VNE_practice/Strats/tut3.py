ff7c5be79a31ccc159266e0c2a5a7a7d46c5143e
Isaac <ismayil.ismayilzada@nokia.com>
2023-11-29 13:27:08 +0100

    small world and vne

--- VNE_practice/Strats/tut3.py
+++ VNE_practice/Strats/tut3.py
@@ -1,35 +1,25 @@
-# NEPTUN.py
-from VNE_classes import Graph, Claim
+#neptun
+from VNE_classes import Graph,Claim
 
 def decision(claim, G, nodes_cap, nodes_load):
-    """
-    Makes a decision on whether to serve a request and how to map it.
-    Args:
-    - claim: The request claim with details of the graph.
-    - G: The substrate graph.
-    - nodes_cap: List of capacities of each node in the substrate graph.
-    - nodes_load: Current load on each node in the substrate graph.
-    Returns:
-    - A list of node indices in the substrate graph if the claim is to be served, otherwise None.
-    """
-    v = 0
-    mapping = []
+    v=0
+    mapping=[]
     for u in range(claim.G.n):
         while v < G.n:
-            if claim.nodes_perf[u] <= nodes_cap[v] - nodes_load[v]:
+            if claim.nodes_perf[u] <= nodes_cap[v]-nodes_load[v]:
                 mapping.append(v)
-                v += 1
+                v+=1
                 break
-            v += 1
-
+            v+=1
     if len(mapping) < claim.G.n:
+        #print claim.G.n, mapping
         return None
-
-    price = 0.0
+    price=0.0
+    #print G.n, mapping, claim.G.E
     for e in claim.G.E:
-        price += 10 * G.D[mapping[e[0]]][mapping[e[1]]]
-
+        price += 10*G.D[mapping[e[0]]][mapping[e[1]]]
+    #print mapping, price, claim.value
     if price < claim.value:
+        #print mapping
         return mapping
-
     return None
